4287b3cacc97c70425dca949f1adef26e7217f54
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2025-03-10 12:41:45 +0100

    js-7

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,28 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", hadleSubmit);
+
+
+function hadleSubmit(event) {
+event.preventDefault()
+const elements = event.target.elements;
+const info = {
+    email: elements.email.value.trim(),
+    password: elements.password.value.trim() 
+}
+
+if (!info.email || !info.password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  console.log(info);
+form.reset();
+};
+
+
+
+
+
+
+
+
 
